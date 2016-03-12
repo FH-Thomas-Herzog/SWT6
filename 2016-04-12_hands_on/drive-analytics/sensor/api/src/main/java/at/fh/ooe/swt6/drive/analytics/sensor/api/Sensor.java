7574e7c12f371fa0aeaa9bb93a8e258a52c67669
Thomas Herzog <herzog.thomas81@gmail.com>
2016-03-12 20:25:10 +0000

    started hands on 1
    - created project structure and maven/eclipse projects
    - implemented needed interfaces and dummy sensor implementations
    - copied instructions to *.adoc since where only available via moodle

---
+++ 2016-04-12_hands_on/drive-analytics/sensor/api/src/main/java/at/fh/ooe/swt6/drive/analytics/sensor/api/Sensor.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package at.fh.ooe.swt6.drive.analytics.sensor.api;
+
+import java.io.Serializable;
+
+/**
+ * This class specifies a sensor.
+ * 
+ * @author Thomas Herzog <S1310307011@students.fh-hagenberg.at>
+ * @date Mar 12, 2016
+ */
+public interface Sensor extends Serializable {
+
+	/**
+	 * Enumeration which specifies the data format a sensor provides.
+	 * 
+	 * @author Thomas Herzog <S1310307011@students.fh-hagenberg.at>
+	 * @date Mar 12, 2016
+	 */
+	public static enum SensorDataFormat {
+		PERCENT, ABSOLUTE_VALUE_LONG;
+	}
+
+	/**
+	 * @return the unique sensor id
+	 */
+	String getSensorId();
+
+	/**
+	 * @return the byte data representing the sensor result
+	 */
+	byte[] getData();
+
+	/**
+	 * @return the sensor used data format.
+	 */
+	SensorDataFormat getDataFormat();
+}
---
+++ 2016-04-12_hands_on/drive-analytics/sensor/impl/nfc/src/main/java/at/fh/ooe/swt6/drive/analytics/sensor/impl/nfc/NfcSensor.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package at.fh.ooe.swt6.drive.analytics.sensor.impl.nfc;
+
+import at.fh.ooe.swt6.drive.analytics.sensor.api.Sensor;
+
+/**
+ * @author Thomas Herzog <S1310307011@students.fh-hagenberg.at>
+ * @date Mar 12, 2016
+ */
+public class NfcSensor implements Sensor {
+
+	private static final long serialVersionUID = 2542405301669418148L;
+
+	private final String id;
+
+	public NfcSensor(String id) {
+		super();
+		this.id = id;
+	}
+
+	@Override
+	public String getSensorId() {
+		return id;
+	}
+
+	@Override
+	public byte[] getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SensorDataFormat getDataFormat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NfcSensor other = (NfcSensor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
